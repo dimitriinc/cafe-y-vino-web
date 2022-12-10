405da9002581859f4fd40d159ff9e9b3535a7343
dimitriinc <dimitriinc@proton.me>
2022-12-09 21:10:56 -0500

    mobile nav menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,22 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const headerMenu = document.querySelector('.header__menu');
+const body = document.querySelector('body');
 
 btnHamburger.addEventListener('click', function() {
-    if (header.classList.contains('open')) {
+    if (header.classList.contains('open')) { // Hamburger closes
+        body.classList.remove('noscroll');
         header.classList.remove('open');
         overlay.classList.add('slide-out');
         overlay.classList.remove('slide-in');
+        // headerMenu.classList.add('has-fade');
         
-    } else {
+    } else { // Hamburger opens
+        body.classList.add('noscroll');
         header.classList.add('open');
         overlay.classList.add('slide-in');
         overlay.classList.remove('slide-out');
+        // headerMenu.classList.remove('has-fade');
     }
 })
