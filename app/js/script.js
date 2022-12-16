df6c8b6e156e54e81e8ab60ce17c0b3aaead4c76
dimitriinc <dimitriinc@proton.me>
2022-12-16 13:13:28 -0500

    overlay links, footer

--- app/js/script.js
+++ app/js/script.js
@@ -1,27 +1,30 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
-const overlay = document.querySelectorAll('.mobile-nav-menu');
-const headerMenu = document.querySelector('.header__menu');
+const overlay = document.querySelector('.mobile-nav-menu');
 const body = document.querySelector('body');
+const mobileNav = document.querySelectorAll('.slider-links');
 
 btnHamburger.addEventListener('click', function() {
     if (header.classList.contains('open')) { // Hamburger closes
         body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.forEach(function(element) {
-            element.classList.add('slide-out');
-            element.classList.remove('slide-in');
+        overlay.classList.add('slide-out');
+        overlay.classList.remove('slide-in');
+        mobileNav.forEach(function(element) {
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
         })
-        // headerMenu.classList.add('has-fade');
+        
         
     } else { // Hamburger opens
         body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.forEach(function(element) {
-            element.classList.add('slide-in');
-            element.classList.remove('slide-out');
+        overlay.classList.add('slide-in');
+        overlay.classList.remove('slide-out');
+        mobileNav.forEach(function(element) {
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
         })
         
-        // headerMenu.classList.remove('has-fade');
     }
 })
