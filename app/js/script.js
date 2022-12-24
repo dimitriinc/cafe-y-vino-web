7748b49252981db8b09203cc1e3c23818f7795f9
dimitriinc <dimitriinc@proton.me>
2022-12-24 12:00:41 -0500

    optimized social icons + animated signature

--- app/js/script.js
+++ app/js/script.js
@@ -4,11 +4,14 @@ const overlay = document.querySelector('.mobile-nav-menu');
 const body = document.querySelector('body');
 const mobileNav = document.querySelectorAll('.slider-links');
 const revealables = document.querySelectorAll('.revealable');
+const dimitriinc = document.querySelector('#dimitriinc');
+const dimiEmail = document.querySelector('#dimi_email');
 
 window.onload = (event) => {
     revealables.forEach(function(element) {
         element.classList.add('revealed');
     })
+    dimitriinc.classList.add('emerged')
 };
 
 window.addEventListener('scroll', function() {
@@ -19,6 +22,20 @@ window.addEventListener('scroll', function() {
             header.classList.remove('scroll');
         }
     }
+})
+
+dimitriinc.addEventListener('click', function() {
+    dimitriinc.classList.add('submerged')
+    dimitriinc.classList.remove('emerged')
+    dimiEmail.classList.add('emerged')
+    dimiEmail.classList.remove('submerged')
+})
+
+dimiEmail.addEventListener('click', function() {
+    dimiEmail.classList.add('submerged')
+    dimitriinc.classList.add('emerged')
+    dimiEmail.classList.remove('emerged')
+    dimitriinc.classList.remove('submerged')
 })
 
 
