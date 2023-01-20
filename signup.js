32fd81537966c95db7ebf76ae56ddc8dd1a20842
dimitriinc <dimitriinc@proton.me>
2023-01-20 18:42:21 -0500

    signup submit anim

--- signup.js
+++ signup.js
@@ -2,6 +2,9 @@ const form = document.getElementById('signup-form');
 const nameInput = document.getElementById('signup-name');
 const emailInput = document.getElementById('signup-email');
 
+const submit_btn = document.getElementById('signup-submit-btn');
+const submit_anim = document.getElementById('loader');
+
 const firebaseConfig = {
     apiKey: "AIzaSyC8URyjiTFzhzOwuJYtftqN0sFaDGzj9rc",
     authDomain: "cafe-y-vino.firebaseapp.com",
@@ -16,23 +19,36 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const fStore = firebase.firestore();
 
+submit_anim.addEventListener('click', () => {
+    submit_anim.setAttribute('style', 'display:none;');
+        submit_btn.removeAttribute('style');
+})
+
 form.addEventListener('submit', signupUserForEmailList);
 
 function signupUserForEmailList(event) {
     event.preventDefault();
+
+    submit_btn.setAttribute('style', 'display:none;');
+    submit_anim.removeAttribute('style');
+
     let userName = nameInput.value;
     let userEmail = emailInput.value;
     console.log(`name: ${userName}`);
     console.log(`email: ${userEmail}`);
-    
-    fStore.collection('mailing-list').add({
-        nombre: userName,
-        email: userEmail
-    }).then(event => {
-        alert('La inscripción exitosa!');
-        window.location.href = '/index.html';
-    }).catch(event => {
-        alert('Caramba!')
-    });
+
+    // fStore.collection('mailing-list').add({
+    //     nombre: userName,
+    //     email: userEmail
+    // }).then(event => {
+    //     submit_anim.setAttribute('style', 'display:none;');
+    //     submit_btn.removeAttribute('style');
+    //     alert('La inscripción exitosa!');
+    //     window.location.href = '/index.html';
+    // }).catch(event => {
+    //     submit_anim.setAttribute('style', 'display:none;');
+    //     submit_btn.removeAttribute('style');
+    //     alert('Caramba!')
+    // });
 
 }
