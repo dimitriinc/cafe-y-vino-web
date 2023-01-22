f6a625e633c8216d8894e438fa73419e2cc35030
dimitriinc <dimitriinc@proton.me>
2023-01-22 12:25:20 -0500

    hero travel

--- scroll-hero.js
+++ scroll-hero.js
@@ -2,8 +2,11 @@ const hero_image = document.querySelector('.hero__image');
 
 
 hero_image.addEventListener('click', () => {
-    window.scrollTo({
-        top: window.scrollY + window.innerHeight - 100,
-        behavior: 'smooth'
-    });
+
+    if (window.pageYOffset === 0) {
+        window.scrollTo({
+            top: window.scrollY + window.innerHeight - 100,
+            behavior: 'smooth'
+        });
+    }
 });
