ee91407d29aa1c8703de19a14533b14684d052e1
dimitriinc <dimitriinc@proton.me>
2023-01-23 11:38:06 -0500

    size check

--- jobs.js
+++ jobs.js
@@ -3,8 +3,6 @@ const input_button = document.querySelector('.input-file-receiver-btn');
 const input_display = document.querySelector('.input-file-receiver-selected');
 const select = document.querySelector('select');
 
-let file_extension;
-
 console.log(`select's initial value: ${select.value}`)
 if (select.value == '') {
     select.setAttribute('style', 'color:grey');
@@ -18,14 +16,19 @@ input_button.addEventListener('click', () => {
 });
 
 input_file.addEventListener('change', (e) => {
-    let file_name = e.target.files[0].name;
-    file_extension = file_name.split('.').pop().toLowerCase();
-    
-    console.log(`file name: ${file_name}\nfile's extension: ${file_extension}`)
+    let file_name = input_file.files[0].name;
+    let file_extension = file_name.split('.').pop().toLowerCase();
 
     if (file_extension === 'pdf') {
         input_display.innerHTML = `<em>${file_name}</em>`;
     } else {
         input_display.innerHTML = `<em>Por favor, selecciona un archivo PDF</em>`;
+        input_file.value = '';
+        return;
+    }
+
+    if (input_file.files[0].size > 5242880) {
+        input_display.innerHTML = `<em>El tamaño no puede ser más de 5MB</em>`;
+        input_file.value = '';
     }
 });
