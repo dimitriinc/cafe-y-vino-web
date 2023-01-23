d55d2f8734ed5c96cef1624f1bf836e092bba6b5
dimitriinc <dimitriinc@proton.me>
2023-01-23 13:27:46 -0500

    test

--- jobs.js
+++ jobs.js
@@ -1,9 +1,29 @@
-const input_file = document.getElementById('file_pdf');
+const input_file = document.getElementById('file-pdf');
 const input_button = document.querySelector('.input-file-receiver-btn');
 const input_display = document.querySelector('.input-file-receiver-selected');
 const select = document.querySelector('select');
+const form_jobs = document.getElementById('form-jobs');
+const jobs_name = document.getElementById('jobs-name');
+const jobs_tel = document.getElementById('jobs-tel');
+const jobs_carta = document.getElementById('jobs-carta');
 
-console.log(`select's initial value: ${select.value}`)
+const jobs_submit_btn = document.getElementById('jobs-submit');
+const jobs_loader = document.getElementById('loader');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8URyjiTFzhzOwuJYtftqN0sFaDGzj9rc",
+    authDomain: "cafe-y-vino.firebaseapp.com",
+    databaseURL: "https://cafe-y-vino-default-rtdb.firebaseio.com",
+    projectId: "cafe-y-vino",
+    storageBucket: "cafe-y-vino.appspot.com",
+    messagingSenderId: "1096226926741",
+    appId: "1:1096226926741:web:d5c23cb2bbba3fb4796b9c",
+    measurementId: "G-D0VKYKE89E"
+};
+
+firebase.initializeApp(firebaseConfig);
+const fStore = firebase.firestore();
+
 if (select.value == '') {
     select.setAttribute('style', 'color:grey');
 }
@@ -31,4 +51,39 @@ input_file.addEventListener('change', (e) => {
         input_display.innerHTML = `<em>El tamaño no puede ser más de 5MB</em>`;
         input_file.value = '';
     }
+});
+
+form_jobs.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (input_file.value == '') {
+        return;
+    }
+
+    jobs_submit_btn.setAttribute('style', 'display:none;');
+    jobs_loader.removeAttribute('style');
+
+    let userName = jobs_name.value;
+    let userTel = jobs_tel.value;
+    let position = select.value;
+    let carta = jobs_carta.value;
+    let cv_file = input_file.files[0];
+
+    console.log(`CV file: ${cv_file}`);
+
+    fStore.collection('aplicaciones_de_trabajo').add({
+        nombre: userName,
+        telefono: userTel,
+        posicion: position,
+        carta: carta
+    }).then(() => {
+        jobs_loader.setAttribute('style', 'display:none;');
+        jobs_submit_btn.removeAttribute('style');
+        alert('Gracias por tu aplicaciòn! Nos contactaremos pronto.')
+        window.location.href = '/index.html';
+    }).catch(() => {
+        jobs_loader.setAttribute('style', 'display:none;');
+        jobs_submit_btn.removeAttribute('style');
+        alert('Caramba!');
+    });
 });
