6a787247cfdfc2b2ea3ce578e2b9030e987d15e0
dimitriinc <dimitriinc@proton.me>
2023-03-17 10:13:25 -0500

    hero scrolling smooth

--- scroll-hero.js
+++ scroll-hero.js
@@ -1,17 +1,21 @@
 const hero_image = document.querySelector('.hero__image');
+const main_text = document.querySelector('.main-text')
+
+const coords = main_text.getBoundingClientRect()
 
 
 hero_image.addEventListener('click', () => {
 
     if (window.pageYOffset === 0) {
         if (document.documentElement.clientWidth > 769) {
-            window.scrollTo({
-                top: window.scrollY + window.innerHeight,
-                behavior: 'smooth'
-            });
+            main_text.scrollIntoView({
+                behavior: 'smooth',
+                block: 'end'
+            })
         } else {
             window.scrollTo({
-                top: window.scrollY + window.innerHeight,
+                left: coords.left + window.pageXOffset,
+                top: coords.top + window.pageYOffset - 80,
                 behavior: 'smooth'
             });
         }
