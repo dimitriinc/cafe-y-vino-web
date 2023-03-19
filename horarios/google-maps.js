8fa6c3071113e580b71eee4bbb339e13757d8a76
dimitriinc <dimitriinc@proton.me>
2023-03-19 12:14:03 -0500

    loader .svg, revealable section cont.

--- horarios/google-maps.js
+++ horarios/google-maps.js
@@ -1,15 +1,25 @@
 
-function initMap() {
-    let myLatLng = {lat: -16.398547309336884, lng: -71.53536741850829};
+// function initMap() {
+//     let myLatLng = {lat: -16.398547309336884, lng: -71.53536741850829};
 
-    let map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: myLatLng
-    });
+//     let map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 12,
+//         center: myLatLng
+//     });
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Café y Vino'
-    });
-}
+//     var marker = new google.maps.Marker({
+//         position: myLatLng,
+//         map: map,
+//         title: 'Café y Vino'
+//     });
+// }
+
+const map = document.querySelector('.gmap')
+const animation = document.querySelector('.gmap-animation')
+
+map.style.opacity = 0
+
+map.addEventListener('load', () => {
+    animation.style.opacity = 0
+    map.style.opacity = 1
+})
