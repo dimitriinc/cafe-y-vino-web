de6e782a4d9df4e5c77360383c94530417cd97ba
dimitriinc <dimitriinc@proton.me>
2024-08-12 12:43:14 -0500

    creds

--- app/js/menu.js
+++ app/js/menu.js
@@ -27,14 +27,14 @@ class Menu {
     _db
     _storageReference
     _firebaseConfig = {
-        apiKey: "AIzaSyC8URyjiTFzhzOwuJYtftqN0sFaDGzj9rc",
-        authDomain: "cafe-y-vino.firebaseapp.com",
-        databaseURL: "https://cafe-y-vino-default-rtdb.firebaseio.com",
-        projectId: "cafe-y-vino",
-        storageBucket: "cafe-y-vino.appspot.com",
-        messagingSenderId: "1096226926741",
-        appId: "1:1096226926741:web:d5c23cb2bbba3fb4796b9c",
-        measurementId: "G-D0VKYKE89E"
+        apiKey: "redacted",
+        authDomain: "redacted",
+        databaseURL: "redacted",
+        projectId: "redacted",
+        storageBucket: "redacted",
+        messagingSenderId: "redacted",
+        appId: "redacted",
+        measurementId: "redacted"
     }
 
     constructor() {
